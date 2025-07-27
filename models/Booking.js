35c42922fb174733a300b710186c856304f2de0b
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-07-27 18:32:39 +0000

    h

--- models/Booking.js
+++ models/Booking.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema(
     {
@@ -54,4 +54,4 @@ const bookingSchema = new mongoose.Schema(
 );
 
 const Booking = mongoose.model("Booking", bookingSchema);
-export default Booking;
+module.exports = Booking
