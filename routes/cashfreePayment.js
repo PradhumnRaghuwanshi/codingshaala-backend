553c059d0ec33cb241a705c93b3899e8adca3014
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-07-29 10:56:32 +0000

    verify

--- routes/cashfreePayment.js
+++ routes/cashfreePayment.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const  axios  = require("axios");
+const { Cashfree, CFEnvironment } = require("cashfree-pg"); 
+
+const cashfree = new Cashfree(CFEnvironment.SANDBOX, process.env.CASHFREE_CLIENT_ID, process.env.CASHFREE_CLIENT_SECRET);
 
 const router = express.Router();
 router.post("/create-order", async (req, res) => {
@@ -43,18 +46,9 @@ router.get('/verify-order/:id', async(req, res)=>{
     try {
         
         const  orderId  = req.params.id;
-        const response = await axios.post(
-            `https://api.cashfree.com/pg/orders/${orderId}`,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    "x-api-version": "2025-01-01",
-                    "x-client-id": process.env.CASHFREE_CLIENT_ID,
-                    "x-client-secret": process.env.CASHFREE_CLIENT_SECRET,
-                },
-            }
-        );
-        console.log(response.data)
+
+        const response = await cashfree.PGOrderFetchPayments(orderId)
+        console.log('Order fetched successfully:', response.data);
         res.status(200).json( response.data );
     } catch (error) {
         console.error("Error creating order:", error.response?.data || error.message);
